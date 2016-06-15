85ab00d7565c5484b1c90c8af70b85f7056d0956
Jáchym Toušek <enumag@gmail.com>
2016-06-15 08:03:23 +0200

    Add FileTypeTest

---
+++ tests/integration/src/FileTypeTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Integration;
+
+use Arachne\Bootstrap\Configurator;
+use Arachne\Upload\Type\FileType;
+use Codeception\Test\Unit;
+use Mockery;
+use Nette\Http\FileUpload;
+use Symfony\Component\Form\FormFactoryInterface;
+
+/**
+ * @author Jáchym Toušek <enumag@gmail.com>
+ */
+class FileTypeTest extends Unit
+{
+    protected function _before()
+    {
+        $config = new Configurator();
+        $config->setTempDirectory(TEMP_DIR);
+        $config->addConfig(__DIR__ . '/../config/config.neon');
+        $container = $config->createContainer();
+
+        $this->factory = $container->getByType(FormFactoryInterface::class);
+    }
+
+    public function testSetData()
+    {
+        $form = $this->factory->createBuilder(FileType::class)->getForm();
+        $data = $this->createUploadedFileMock('abcdef', 'original.jpg', true);
+        $form->setData($data);
+        $this->assertSame($data, $form->getData());
+    }
+
+    public function testSubmit()
+    {
+        $form = $this->factory->createBuilder(FileType::class)->getForm();
+        $data = $this->createUploadedFileMock('abcdef', 'original.jpg', true);
+        $form->submit($data);
+        $this->assertSame($data, $form->getData());
+    }
+
+    public function testSubmitEmpty()
+    {
+        $form = $this->factory->createBuilder(FileType::class)->getForm();
+        $form->submit(null);
+        $this->assertNull($form->getData());
+    }
+    public function testSubmitMultiple()
+    {
+        $form = $this->factory->createBuilder(FileType::class, null, [
+            'multiple' => true,
+        ])->getForm();
+        $data = [
+            $this->createUploadedFileMock('abcdef', 'first.jpg', true),
+            $this->createUploadedFileMock('zyxwvu', 'second.jpg', true),
+        ];
+        $form->submit($data);
+        $this->assertSame($data, $form->getData());
+        $view = $form->createView();
+        $this->assertSame('arachne_file[]', $view->vars['full_name']);
+        $this->assertArrayHasKey('multiple', $view->vars['attr']);
+    }
+    public function testDontPassValueToView()
+    {
+        $form = $this->factory->create(FileType::class);
+        $form->submit([
+            FileType::class => $this->createUploadedFileMock('abcdef', 'original.jpg', true),
+        ]);
+        $view = $form->createView();
+        $this->assertEquals('', $view->vars['value']);
+    }
+
+    private function createUploadedFileMock($name, $originalName, $valid)
+    {
+        $file = Mockery::mock(FileUpload::class);
+
+        $file
+            ->shouldReceive('getTemporaryFile')
+            ->andReturn($name);
+
+        $file
+            ->shouldReceive('getName')
+            ->andReturn($originalName);
+
+        $file
+            ->shouldReceive('isOk')
+            ->andReturn($valid);
+
+        return $file;
+    }
+}
